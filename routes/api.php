1395ffeed011bedcf0bb12e6634c639c20d657b0
zaenarifputra <91659924+zaenarifputra@users.noreply.github.com>
2022-10-31 15:09:52 +0700

    coba

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\BookController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -21,7 +18,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('me', [AuthController::class, 'me']);
 
 Route::resource('books', BookController::class)->except([
     'create', 'edit'
